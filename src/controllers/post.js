7df2551525b04f45cb7be69d0d0bfc7f5f7583f0
faham-code <fahamafraz@gmail>
2025-10-06 23:25:37 +0530

    Fix wrong route import path (src/src issue)

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -27,6 +27,15 @@ export const createPost = async (req, res) => {
   }
 };
 
+// get all posts
+export const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ createdAt: -1 });
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
 
 
 // Get all posts (for user and their friends)
